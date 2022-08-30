c83d134620389b6f42fa88d9af3c023b24fb16ce
Thorrell Turner <thorrellt@gmail.com>
2022-08-30 07:54:09 -0400

    add images and heading content layout

--- src/components/Footing.jsx
+++ src/components/Footing.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import '../styles/Footer.css'
+import aboutDark from '../assets/image-about-dark.jpg'
+import aboutLight from '../assets/image-about-light.jpg'
 
 
 export default function Footer() {
@@ -7,13 +9,13 @@ export default function Footer() {
     return (
         <section id="Footer">
             <div className="img-1">
-                <h1>Im am Image #1</h1>
+            <img src={aboutDark} alt="about image" />
             </div>
             <div className="content">
                 <h1>Im the Content</h1>
             </div>
             <div className="img-2">
-                <h1>Im am Image #2</h1>
+                <img src={aboutLight} alt="about image" />
             </div>
         </section>
     )
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import '../styles/Heading.css'
+import mainImg1 from '../assets/desktop-image-hero-1.jpg'
+import aboutDark from '../assets/image-about-dark.jpg'
+import aboutLight from '../assets/image-about-light.jpg'
+import arrow from '../assets/icon-arrow.svg'
 
 
 export default function Header() {
@@ -7,10 +11,23 @@ export default function Header() {
     return (
         <section id="Heading">
             <div className="img">
-                <h1>Im the Image</h1>
+                <img src={mainImg1} alt="main image" />
             </div>
             <div className="content">
-                <h1>Im the Content</h1>
+                <h1>
+                    Discover innovative
+                </h1>
+                <p>
+                    We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.
+                </p>
+                <a href="#">
+                    <h3>
+                        shop now
+                    </h3>
+                    <img className='arrow' src={arrow} alt="" />
+                </a>
+
+
             </div>
 
         </section>
