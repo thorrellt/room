4b56e98351e217fb09fba1695f22dc03f5d1e51d
Thorrell Turner <thorrellt@gmail.com>
2022-08-30 08:00:48 -0400

    add footer content

--- src/components/Footing.jsx
+++ src/components/Footing.jsx
@@ -9,10 +9,15 @@ export default function Footer() {
     return (
         <section id="Footer">
             <div className="img-1">
-            <img src={aboutDark} alt="about image" />
+                <img src={aboutDark} alt="about image" />
             </div>
             <div className="content">
-                <h1>Im the Content</h1>
+                <h2>
+                    ABOUT OUR FURNITURE
+                </h2>
+                <p>
+                    Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.
+                </p>
             </div>
             <div className="img-2">
                 <img src={aboutLight} alt="about image" />
