74259fb8f4ee0b36ea20b15febc7a50aba9db360
Thorrell Turner <thorrellt@gmail.com>
2022-09-04 09:37:27 -0400

    animate mobile nav transition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,41 +14,34 @@ export default function Navbar(props) {
         <nav>
             <img src={roomLogo} className='nav-logo' alt="" />
             {windowWidth <= 900 &&
-                <button onClick={toggleNav}>
+                <button onClick={toggleNav} className='nav-toggle-btn'>
                     <img src={navActive ? close : hamburger}
                         className='nav-toggle'
                     />
                 </button>
             }
-                <ul className={navActive ? 'show' : 'hidden'}>
-                {windowWidth <= 900 && navActive &&
-                <button className='nav-toggle-btn' onClick={toggleNav}>
-                    <img src={close}
-                        className='nav-toggle'
-                    />
-                </button>
-            }
-                    <li>
-                        <a href="#">
-                            home
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            shop
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            about
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            contact
-                        </a>
-                    </li>
-                </ul>
+            <ul className={navActive ? '' : 'hidden'}>
+                <li>
+                    <a href="#">
+                        home
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        shop
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        about
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        contact
+                    </a>
+                </li>
+            </ul>
         </nav>
     )
 
